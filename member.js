e5c05d09d83c293cdc765be72947eaf975d722ae
Zhu Tianhao <96347253+TUNGTREE@users.noreply.github.com>
2023-12-30 05:44:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,19 @@
+function skillsMember()
+{
+    var member = new Object();
+    member.name = "Member";
+    member.description = "You are a member of the guild.";
+    member.bonuses = new Object();
+    member.bonuses["skill:alchemy"] = 1;
+    member.bonuses["skill:armourer"] = 1;
+    member.bonuses["skill:bowyer"] = 1;
+    member.bonuses["skill:carpenter"] = 1;
+    member.bonuses["skill:cook"] = 1;
+    member.bonuses["skill:enchanter"] = 1;
+    member.bonuses["skill:farmer"] = 1;
+    member.bonuses["skill:fletcher"] = 1;
+    member.bonuses["skill:herbalist"] = 1;
+    member.bonuses["skill:leatherworker"] = 1;
+    member.bonuses["skill:locksmith"] = 1;
+    member.bonuses["skill:merchant"] = 1;
+    mem
